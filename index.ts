0f811f0d7b3118ef49cc176abc305ae318565faa
davoxpa <xolero@gmail.com>
2023-08-18 11:44:49 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,26 +5,20 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-function IfElse(
-  condition: boolean,
-  trueMethodName: string,
-  falseMethodName: string
-) {
+function IfElse(condition: boolean, falseMethodName: string) {
   return function (
     target: any,
     methodName: string,
     descriptor: PropertyDescriptor
   ) {
     const originalMethod = descriptor.value;
-    descriptor.value = condition
-      ? target[trueMethodName]
-      : target[falseMethodName];
+    descriptor.value = condition ? originalMethod : target[falseMethodName];
     return descriptor;
   };
 }
 
 class ExampleClass {
-  @IfElse(false, 'exampleMethod', 'methodIfFalse')
+  @IfElse(false, 'methodIfFalse')
   exampleMethod() {
     console.log('original method');
     // Original method content (not used)
